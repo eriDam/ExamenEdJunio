9ee2b6087695e28178e8f83da34f775f2c7462a6
erceca <erceca@A18d1.alumnado.flo>
2014-06-19 12:09:23 +0200

    Create new project for ED exams

---
+++ ExamenEd/src/ExamenEd.java
@@ -0,0 +1,14 @@
+
+public class ExamenEd {
+
+	public ExamenEd() {
+		//Apartado 3 Examen Ed
+		System.out.println("Este el es el examen de ED");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
